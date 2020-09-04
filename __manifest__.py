5ccdc87687d4cdc813ecabcd8a2456d89b624742
vikas <vikas.kumar2@oodlestechnologies.com>
2020-09-04 17:44:03 +0530

    first_commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Shopify Connector',
+    'version': '12.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for serial or lot creation',
+    'sequence': '55',
+    'author': 'vikas kumar',
+    'maintainer': 'odoo mates',
+    'website': 'odoomate.com',
+    'depends': ["base", "sale_management", "stock", "account", "mail", "sale", "purchase", 'mrp'],
+    'demo': [],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto-install': False,
+
+}
